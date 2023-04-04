7c645b00836e3f5c33b51b7a145ceaee8c9b865f
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-04-05 00:02:56 +0300

    Dockerized my project

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-# from app import app, db
 
 
 app = Flask(__name__)
